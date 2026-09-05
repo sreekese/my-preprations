bd5722a0ae5161bc964abcc685a53ba533f40b86
sreekese <sreekeseps@gmail.com>
2026-09-05 21:31:51 +0530

    Kadane's Algorithm

---
+++ Kadane-Algorithm.js
@@ -0,0 +1,20 @@
+function maxSubarraySum(nums) {
+    let currentSum = nums[0];
+    let maxSum = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+
+        currentSum = Math.max(
+            nums[i],
+            currentSum + nums[i]
+        );
+
+        maxSum = Math.max(maxSum, currentSum);
+    }
+
+    return maxSum;
+}
+
+console.log(
+    maxSubarraySum([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+);
